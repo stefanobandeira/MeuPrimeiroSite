1294c41f04cbdb05fa32aabb8fc5fae7da134d2d
Stéfano M Bandeira <stefanobandeira333@gmail.com>
2021-06-10 12:56:19 -0300

    meu primeiro commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+//$('.slider-principal').slick();
+
+  $('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2500
+
+   
+  });
